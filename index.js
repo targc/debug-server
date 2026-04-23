86bc9896458a86b7af74e4cd5a356bb8cb3d0b01
targc <sommit.kcc@gmail.com>
2026-04-23 21:53:08 +0700

    script

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.get("/:statusCode", (req, res) => {
   res.status(statusCode).json({ status: statusCode });
 });
 
-const port = 8080;
+const port = 443;
 
 app.listen(port, () => {
   console.log(`running on port :${port}`);
